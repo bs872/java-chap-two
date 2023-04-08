5384cac373c0f5fccea88e90fc84985d459081be
Bismah Shahzad Alam <127497856+bs872@users.noreply.github.com>
2023-04-08 06:08:41 -0700

    Add files via upload

---
+++ java (b)/app.js
@@ -0,0 +1,19 @@
+var username;
+var myName = "BISMAH SHAHZAD ALAM";
+var titledMessage;
+titledMessage = "Hello everyone!";
+alert(titledMessage);
+var fullname = "BISMAH SHAHZAD ALAM";
+var age = 18  + " years old";
+var course = "Certified Mobile & Application Development";
+alert(fullname);
+alert(age);
+alert(course);
+var message = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+alert ( message);
+var emailadress = "bs8720414@gmail.com";
+alert(emailadress);
+var javabook ="i'm trying to learn from the book A Smarter Way to Learn JavaScript";
+alert ( javabook);
+var Symbol = ("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”");
+alert ( Symbol)
